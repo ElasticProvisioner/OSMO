a27a9566b4d1bb0e2bd59c1b3b8234e045203983
Ethan Yu <ethany@nvidia.com>
2025-12-18 13:20:06 -0800

    Add progress for backend events

--- src/operator/backend_listener.py
+++ src/operator/backend_listener.py
@@ -1430,11 +1430,13 @@ def watch_backend_events(progress_writer: progress.ProgressWriter,
             if error.status == 410:
                 # Reset last resource version
                 last_resource_version = ''
+            progress_writer.report_progress()
         except urllib3.exceptions.ReadTimeoutError:
             helpers.send_log_through_queue(
                 backend_messages.LoggingType.WARNING,
                 'Connection timed out during watch backend events, reestablishing watch stream.',
                 event_send_queue)
+            progress_writer.report_progress()
         except (urllib3.exceptions.MaxRetryError, urllib3.exceptions.ProtocolError) as error:
             send_connection_error_count(event_type='backend')
             helpers.send_log_through_queue(
@@ -1442,6 +1444,7 @@ def watch_backend_events(progress_writer: progress.ProgressWriter,
                 f'Cluster monitor errored out during watch backend events due to {error} ' +\
                 'retrying ...',
                 event_send_queue)
+            progress_writer.report_progress()
         except KeyboardInterrupt:
             sys.exit(0)
         except Exception as error:  # pylint: disable=broad-except
